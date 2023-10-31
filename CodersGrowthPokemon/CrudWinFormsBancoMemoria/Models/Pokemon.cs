715f837d7832f38b73011dab88374a4c7a2143a7
Eduardo Lozano <eduardoellozi2023@gmail.com>
2023-10-31 09:13:43 -0300

    Criação do modelo de dados e do Enum para o tipo dos pokemons

---
+++ CodersGrowthPokemon/CrudWinFormsBancoMemoria/Models/Pokemon.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrudWinFormsBancoMemoria.Models
+{
+    public class Pokemon
+    {
+        private int _id { get; set; }
+
+        private string _nome { get; set; }
+
+        private string _apelido { get; set; }
+
+        private int _nivel { get; set; }
+
+        private Boolean _evolucao { get; set; }
+
+        private decimal _altura { get; set; }
+
+        private Boolean _favorito { get; set; }
+
+        private DateTime _dataDeCaptura { get; set; }
+
+        private List<TipoPokemon> _tipo { get; set; }
+
+        public Pokemon()
+        {
+
+        }
+
+        public Pokemon(int id, string nome, string apelido, int nivel, bool evolucao, decimal altura, bool favorito, DateTime dataDeCaptura, List<TipoPokemon> tipo)
+        {
+            _id = id;
+            _nome = nome;
+            _apelido = apelido;
+            _nivel = nivel;
+            _evolucao = evolucao;
+            _altura = altura;
+            _favorito = favorito;
+            _dataDeCaptura = dataDeCaptura;
+            _tipo = tipo;
+        }
+    }
+}
---
+++ CodersGrowthPokemon/CrudWinFormsBancoMemoria/Models/TipoPokemon.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrudWinFormsBancoMemoria.Models
+{
+    public enum TipoPokemon
+    {
+        Água,
+        Fogo,
+        Planta,
+        Elétrico,
+        Gelo,
+        Lutador,
+        Psíquico,
+        Terra,
+        Voador,
+        Veneno,
+        Rocha,
+        Inseto,
+        Fantasma,
+        Noturno,
+        Aço,
+        Dragão
+    }
+}
