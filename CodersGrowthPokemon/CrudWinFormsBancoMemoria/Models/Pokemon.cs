86c5890c86670a87beadbb916c743a0142a5c1d1
Eduardo Lozano <eduardoellozi2023@gmail.com>
2023-10-31 09:54:50 -0300

    Remodelando a classe Pokemon

--- CodersGrowthPokemon/CrudWinFormsBancoMemoria/Models/Pokemon.cs
+++ CodersGrowthPokemon/CrudWinFormsBancoMemoria/Models/Pokemon.cs
@@ -8,40 +8,23 @@ namespace CrudWinFormsBancoMemoria.Models
 {
     public class Pokemon
     {
-        private int _id { get; set; }
+        private int Id { get; set; }
 
-        private string _nome { get; set; }
+        private string Nome { get; set; }
 
-        private string _apelido { get; set; }
+        private string Apelido { get; set; }
 
-        private int _nivel { get; set; }
+        private int Nivel { get; set; }
 
-        private Boolean _evolucao { get; set; }
+        private decimal Altura { get; set; }
 
-        private decimal _altura { get; set; }
+        private Boolean Shiny { get; set; }
 
-        private Boolean _favorito { get; set; }
+        private DateTime DataDeCaptura { get; set; }
 
-        private DateTime _dataDeCaptura { get; set; }
+        private TipoPokemon TipoPrincipal { get; set; }
 
-        private List<TipoPokemon> _tipo { get; set; }
-
-        public Pokemon()
-        {
-
-        }
-
-        public Pokemon(int id, string nome, string apelido, int nivel, bool evolucao, decimal altura, bool favorito, DateTime dataDeCaptura, List<TipoPokemon> tipo)
-        {
-            _id = id;
-            _nome = nome;
-            _apelido = apelido;
-            _nivel = nivel;
-            _evolucao = evolucao;
-            _altura = altura;
-            _favorito = favorito;
-            _dataDeCaptura = dataDeCaptura;
-            _tipo = tipo;
-        }
+        private TipoPokemon? TipoSecundario { get; set; }
+        
     }
 }
