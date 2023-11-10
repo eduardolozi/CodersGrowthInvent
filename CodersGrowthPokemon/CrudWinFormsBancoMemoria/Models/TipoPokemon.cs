33b9d84eccc444089790041e17a7bddc7dbd7296
Eduardo Lozano <eduardoellozi2023@gmail.com>
2023-11-10 13:19:13 -0300

    Consertando o enum

--- CodersGrowthPokemon/CrudWinFormsBancoMemoria/Models/TipoPokemon.cs
+++ CodersGrowthPokemon/CrudWinFormsBancoMemoria/Models/TipoPokemon.cs
@@ -2,22 +2,22 @@
 {
     public enum TipoPokemon
     {
-        Agua,
-        Fogo,
-        Planta,
-        Eletrico,
-        Gelo,
-        Lutador,
-        Psiquico,
-        Terra,
-        Voador,
-        Veneno,
-        Rocha,
-        Inseto,
-        Fantasma,
-        Noturno,
-        Aco,
-        Dragao, 
-        Normal
+        Agua = 1,
+        Fogo = 2,
+        Planta = 3,
+        Eletrico = 4,
+        Gelo = 5,
+        Lutador = 6,
+        Psiquico = 7,
+        Terra = 8,
+        Voador = 9,
+        Veneno = 10,
+        Rocha = 11,
+        Inseto = 12,
+        Fantasma = 13, 
+        Noturno = 14,
+        Aco = 15,
+        Dragao = 16, 
+        Normal = 17
     }
 }
