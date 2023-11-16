6acbab8e04787d094fa78a9e15a09309882f02e4
Eduardo Lozano <eduardoellozi2023@gmail.com>
2023-11-16 14:17:32 -0300

    criando uma classe para a conexao

--- CodersGrowthPokemon/CrudWinFormsBancoMemoria/Program.cs
+++ CodersGrowthPokemon/CrudWinFormsBancoMemoria/Program.cs
@@ -10,31 +10,16 @@ namespace CrudWinFormsBancoMemoria
         [STAThread]
         static void Main()
         {
-            using (var serviceProvider = CreateServices())
-            using (var scope = serviceProvider.CreateScope())
+            try
             {
-                UpdateDatabase(scope.ServiceProvider);
+                BancoDeDados.ConfiguracaoDaMigracao();
+            } catch (Exception ex)
+            {
+                ex.ToString(); 
             }
             ApplicationConfiguration.Initialize();
             Application.Run(new GerenciadorDePokemons());
         }
 
-        private static ServiceProvider CreateServices()
-        {
-            return new ServiceCollection()
-                .AddFluentMigratorCore()
-                .ConfigureRunner(rb => rb
-                    .AddSqlServer()
-                    .WithGlobalConnectionString(@"Data Source=DESKTOP-O3F8OCR;Initial Catalog=CodersGrowth;Persist Security Info=True;User ID=sa;Password=sap@123")
-                    .ScanIn(typeof(AddPokemonTable).Assembly).For.Migrations())
-                .AddLogging(lb => lb.AddFluentMigratorConsole())
-                .BuildServiceProvider(false);
-        }
-
-        private static void UpdateDatabase(IServiceProvider serviceProvider)
-        {
-            var runner = serviceProvider.GetRequiredService<IMigrationRunner>();
-            runner.MigrateUp();
-        }
     }
 }
