542a731fa5572c21ca4f6eab1fbe8754c1949d3f
Eduardo Lozano <eduardoellozi2023@gmail.com>
2023-12-07 14:25:48 -0300

    criando a funcao de fetch para listar os pokemons

--- CodersGrowthPokemon/webapp/wwwroot/Controller/Listagem.controller.js
+++ CodersGrowthPokemon/webapp/wwwroot/Controller/Listagem.controller.js
@@ -1,5 +1,13 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller"
 ], (Controller) => {
-    return Controller.extend("webapp.Controller.Listagem", {})
+    return Controller.extend("webapp.Controller.Listagem", {
+        onInit() {
+            const pokemonsJSON = fetch("http://localhost:7237/pokemons")
+                .then(response => response.json)
+                .catch(() => console.log("Erro ao pegar os pokemons do banco de dados."))
+                .then(() => console.log(pokemonsJSON))
+            this.getView().setModel(pokemonsJSON, "view");
+        }
+    });
 });
