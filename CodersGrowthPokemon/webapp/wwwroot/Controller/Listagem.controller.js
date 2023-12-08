06af23bf888963fd10b98885d5bd92fba5a4be30
Eduardo Lozano <eduardoellozi2023@gmail.com>
2023-12-08 09:48:08 -0300

    consertando o binding do i18n

--- CodersGrowthPokemon/webapp/wwwroot/Controller/Listagem.controller.js
+++ CodersGrowthPokemon/webapp/wwwroot/Controller/Listagem.controller.js
@@ -1,28 +1,22 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
-    "sap/ui/model/json/JSONModel"
+    "sap/ui/model/json/JSONModel",
+    "../model/formatter"
 ], (Controller, JSONModel) => {
     "use strict"
     return Controller.extend("webapp.Controller.Listagem", {
-        onInit() {
-            // const pokemonsJSON = fetch("https://localhost:7237/pokemons")
-            //     .then(response => {
-            //         if(!response.ok) return new Error("Requisicao falhou");
-            //         if(response.status === 404) return new Error("Nao encontrou resultados");
-            //         return response.json
-            //     })
-            // pokemonsJSON.then((e) => this.getView().setModel(pokemonsJSON, "view")).catch((e) => console.log("errooooooo" + e.message));
 
-            
+        onInit() {
+            this._carregarPokemons();
         },
 
-        carregarPokemons() {
+        _carregarPokemons() {
             fetch("/pokemons")
                 .then(response => {
                     return response.json();
                 })
                 .then(response => {
-                   this.getView().setModel(new JSONModel(response, "pokemons")) 
+                    this.getView().setModel(new JSONModel(response), "pokemons");
                 })
                 .catch(error => {
                     console.log(error);
---
+++ CodersGrowthPokemon/webapp/wwwroot/model/formatter.js
@@ -0,0 +1,14 @@
+// sap.ui.define([
+//     "sap.ui.core.format.DateFormat"
+// ], (DateFormat) => {
+// 	"use strict";
+
+// 	return {
+// 		formatarData(data) {
+//             console.log(data)
+// 			const pokemon = this.getOwnerComponent.getModel("pokemons");
+//             console.log(pokemon);
+            
+//         }
+//     };
+// });
