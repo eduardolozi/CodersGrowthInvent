177abc1a2de9dfa5adb6f5a47951be196269431a
Eduardo Lozano <eduardoellozi2023@gmail.com>
2023-12-08 12:17:18 -0300

    feita a conversao do valor de data de captura

--- CodersGrowthPokemon/webapp/wwwroot/model/formatter.js
+++ CodersGrowthPokemon/webapp/wwwroot/model/formatter.js
@@ -1,14 +1,13 @@
-// sap.ui.define([
-//     "sap.ui.core.format.DateFormat"
-// ], (DateFormat) => {
-// 	"use strict";
+sap.ui.define([
+], () => {
+"use strict";
 
-// 	return {
-// 		formatarData(data) {
-//             console.log(data)
-// 			const pokemon = this.getOwnerComponent.getModel("pokemons");
-//             console.log(pokemon);
+return {
+    formatarData(data) {
+            console.log(data)
+        const pokemon = this.getOwnerComponent.getModel("pokemons");
+            console.log(pokemon);
             
-//         }
-//     };
-// });
+        }
+    };
+});
