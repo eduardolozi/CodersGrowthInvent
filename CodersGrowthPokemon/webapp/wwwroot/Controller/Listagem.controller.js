b4f387f52b5353d66b882c7124e79a6564ecbc93
Eduardo Lozano <eduardoellozi2023@gmail.com>
2023-12-08 08:21:04 -0300

    fazendo o fetch

--- CodersGrowthPokemon/webapp/wwwroot/Controller/Listagem.controller.js
+++ CodersGrowthPokemon/webapp/wwwroot/Controller/Listagem.controller.js
@@ -1,13 +1,32 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-], (Controller) => {
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/model/json/JSONModel"
+], (Controller, JSONModel) => {
+    "use strict"
     return Controller.extend("webapp.Controller.Listagem", {
         onInit() {
-            const pokemonsJSON = fetch("http://localhost:7237/pokemons")
-                .then(response => response.json)
-                .catch(() => console.log("Erro ao pegar os pokemons do banco de dados."))
-                .then(() => console.log(pokemonsJSON))
-            this.getView().setModel(pokemonsJSON, "view");
+            // const pokemonsJSON = fetch("https://localhost:7237/pokemons")
+            //     .then(response => {
+            //         if(!response.ok) return new Error("Requisicao falhou");
+            //         if(response.status === 404) return new Error("Nao encontrou resultados");
+            //         return response.json
+            //     })
+            // pokemonsJSON.then((e) => this.getView().setModel(pokemonsJSON, "view")).catch((e) => console.log("errooooooo" + e.message));
+
+            
+        },
+
+        carregarPokemons() {
+            fetch("/pokemons")
+                .then(response => {
+                    return response.json();
+                })
+                .then(response => {
+                   this.getView().setModel(new JSONModel(response, "pokemons")) 
+                })
+                .catch(error => {
+                    console.log(error);
+                });
         }
     });
-});
+})
