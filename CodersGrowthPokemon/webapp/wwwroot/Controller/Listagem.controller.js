5d339806c5b4dd49567e865f0c448e6591954946
Eduardo Lozano <eduardoellozi2023@gmail.com>
2023-12-12 08:35:31 -0300

    implementando funcao para converter base64 para blob

--- CodersGrowthPokemon/webapp/wwwroot/Controller/Listagem.controller.js
+++ CodersGrowthPokemon/webapp/wwwroot/Controller/Listagem.controller.js
@@ -16,12 +16,44 @@ sap.ui.define([
                     return response.json();
                 })
                 .then(response => {
+                    Object.entries(reponse).forEach(([key, value]) => {
+                        console.log(response);
+                        if(key=='foto') {
+                            value = this._converteBase64ParaBlob(value);
+                            console.log(value);
+                            value = URL.createObjectURL(value);
+                            console.log(value);
+                        }
+                    })
+                    
                     this.getView().setModel(new JSONModel(response), "pokemons");
                 })
                 .catch(error => {
                     console.log(error);
                 });
-        }
+        },
+
+        _converteBase64ParaBlob(b64Data, contentType='', sliceSize=512) {
+            const byteCharacters = atob(b64Data);
+            const byteArrays = [];
+          
+            for (let offset = 0; offset < byteCharacters.length; offset += sliceSize) {
+              const slice = byteCharacters.slice(offset, offset + sliceSize);
+          
+              const byteNumbers = new Array(slice.length);
+              for (let i = 0; i < slice.length; i++) {
+                byteNumbers[i] = slice.charCodeAt(i);
+              }
+          
+              const byteArray = new Uint8Array(byteNumbers);
+              byteArrays.push(byteArray);
+            }
+          
+            const blob = new Blob(byteArrays, {type: contentType});
+            return blob;
+          }
+
+
 
     });
 })
--- CodersGrowthPokemon/webapp/wwwroot/model/formatter.js
+++ CodersGrowthPokemon/webapp/wwwroot/model/formatter.js
@@ -8,53 +8,9 @@ sap.ui.define([
         },
 
         formataTipo(tipo) {            
-            switch(tipo) {
-                case 1:
-                    return "Agua";
-                case 2:
-                    return "Fogo";
-                case 3:
-                    return "Planta";
-                case 4:
-                    return "Eletrico";
-                case 5:
-                    return "Gelo";
-                case 6:
-                    return "Lutador";
-                case 7:
-                    return "Psiquico";
-                case 8:
-                    return "Terra";
-                case 9:
-                    return "Voador";
-                case 10:
-                    return "Veneno";
-                case 11:
-                    return "Rocha";
-                case 12:
-                    return "Inseto";
-                case 13:
-                    return "Fantasma";
-                case 14:
-                    return "Noturno";
-                case 15:
-                    return "Aco";
-                case 16:
-                    return "Dragao";
-                case 17:
-                    return "Normal";
-                default:
-                    return "Nenhum";
-            }
-        },
-
-        formataImagem(base64) {
-            let urlCreator = window.URL || window.webkitURL;
-            let url = urlCreator.createObjectURL(base64);
-            let foto = xmlDoc.createElement("ImagemPokemon");
-            foto.src = url;
-            let elemento = xmlDoc.getElementsByTagName("FlexBox");
-            elemento[0].appendChild(foto)
+        const tipos = ["Água", "Fogo", "Planta", "Elétrico", "Gelo", "Lutador", "Psíquico", "Terra", "Voador", "Veneno", "Rocha", "Inseto", "Fantasma", "Noturno", "Aço", "Dragão", "Normal"];
+        const INDEX_DO_POKEMON = tipo - 1;
+        return ((tipo < 1 || tipo > 17) ? "Nenhum" : tipos[INDEX_DO_POKEMON]); 
         }
     }
 })
