da84c6569be71e04385824f4aba7bc0164b1bc36
Eduardo Lozano <eduardoellozi2023@gmail.com>
2023-12-18 13:54:47 -0300

    feat: tela de cadastro faltando apenas a finalizacao do fetch. o design ja foi feito e as navagaçoes tambem

--- CodersGrowthPokemon/webapp/wwwroot/Controller/Cadastro.controller.js
+++ CodersGrowthPokemon/webapp/wwwroot/Controller/Cadastro.controller.js
@@ -1,14 +1,34 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
     "../model/formatter",
-    "sap/ui/core/routing/History"
-], (Controller, formatter, History) => {
+    "sap/ui/core/routing/History",
+    "sap/m/MessageBox",
+    "sap/ui/model/json/JSONModel"
+], (Controller, formatter, History, MessageBox, JSONModel) => {
     "use strict"
+    const sim = "Sim";
+    const nao = "Não";
+    const nomeModeloPokemon = "pokemon"
     return Controller.extend("webapp.Controller.Cadastro", {
         formatter: formatter,
 
         onInit() {
+            this.getView().setModel(new JSONModel({}), nomeModeloPokemon);
+        },
 
+        _salvarPokemon() {
+            const urlFetch = "/pokemons"
+            const dados = JSON.stringify(this.getView().getModel(nomeModeloPokemon).getData())
+            dados.
+            dados.shiny = this.getView().byId("inputShiny").getSelected();
+            console.log(dados)
+            fetch(urlFetch, {
+                method: "POST",
+            }).then(response => {
+                return response.json();
+            }).then(response => {
+                console.log(response)
+            })
         },
 
         aoClicarNoBotaoDeVoltar(oEvent) {
@@ -23,6 +43,35 @@ sap.ui.define([
 				const oRouter = this.getOwnerComponent().getRouter();
 				oRouter.navTo(paginaDeListagem, {}, true);
 			}
+        },
+
+        aoClicarNoBotaoDeSalvar(oEvent) {
+            const mensagemAoClicarEmSalvar = "Salvar o Pokémon criado?";
+
+            MessageBox.information(mensagemAoClicarEmSalvar, {
+                actions: [sim, nao],
+                emphasizedAction: sim,
+                onClose: (oAction) => {
+                    if (oAction === sim) {
+                        this._salvarPokemon();
+                        this.aoClicarNoBotaoDeVoltar();
+                    } 
+                }
+            });
+        },
+
+        aoClicarNoBotaoDeCancelar(oEvent) {
+            const mensagemAoClicarEmCancelar = "Cancelar a criação do Pokémon?"
+
+            MessageBox.alert(mensagemAoClicarEmCancelar, {
+                actions: [sim, nao],
+                emphasizedAction: sim,
+                onClose: (oAction) => {
+                    if (oAction === sim) {
+                        this.aoClicarNoBotaoDeVoltar();
+                    } 
+                }
+            });
         }
 
     });
