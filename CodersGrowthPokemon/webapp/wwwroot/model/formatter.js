3e53a685891028518b16053454eb37af4a3c2f13
Eduardo Lozano <eduardoellozi2023@gmail.com>
2023-12-19 14:00:39 -0300

    criando o conversor de img para base64

--- CodersGrowthPokemon/webapp/wwwroot/model/formatter.js
+++ CodersGrowthPokemon/webapp/wwwroot/model/formatter.js
@@ -23,8 +23,20 @@ sap.ui.define([
             return (foto) ? foto : urlImagemIndefinida;
         },
 
-        converteUrlParaBase64(url) {
-            
+        converteUrlParaBase64(url, callback) {
+            const image = new Image();
+            image.crossOrigin='anonymous';
+            image.onload = () => {
+                const canvas = document.createElement('canvas');
+                const ctx = canvas.getContext('2d');
+                canvas.height = image.naturalHeight;
+                canvas.width = image.naturalWidth;
+                ctx.drawImage(image, 0, 0);
+                const dataUrl = canvas.toDataURL();
+                callback && callback(dataUrl)
+            }
+            image.src = url;
+            return image.src;
         }
     }
 })
